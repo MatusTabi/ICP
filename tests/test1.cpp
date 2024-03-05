1d292788974fcbc55383bcfcd55e748300c6706b
MatusTabi <matustabi44@gmail.com>
2024-03-05 21:27:36 +0100

    test: added more tests for moving and rotating

--- tests/test1.cpp
+++ tests/test1.cpp
@@ -1,8 +1,36 @@
 #include <gtest/gtest.h>
 #include "../src/models/robot.h"
 
-TEST(AddTest, AddsTwoIntegers) {
-    EXPECT_EQ(Robot::sum(3, 9), 12);
+#include <vector>
+#include <iostream>
+
+class SimpleTest : public testing::Test {
+    protected:
+        Robot *robot;
+
+        void SetUp() override {
+            robot = new Robot(QPoint(100, 100), 2, Direction::LEFT);
+        }
+};
+
+TEST_F(SimpleTest, initialization_test) {
+    EXPECT_EQ(robot->get_direction(), Direction::LEFT);
+    EXPECT_EQ(robot->get_position(), QPoint(100, 100));
+    EXPECT_EQ(robot->get_speed(), 2);
+}
+
+TEST_F(SimpleTest, simple_single_move_test) {
+    robot->move();
+    EXPECT_EQ(robot->get_position(), QPoint(98, 100));
+}
+
+TEST_F(SimpleTest, move_with_rotation_test) {
+    robot->move();
+    EXPECT_EQ(robot->get_position(), QPoint(98, 100));
+    robot->rotate();
+    EXPECT_EQ(robot->get_direction(), Direction::UP);
+    robot->move();
+    EXPECT_EQ(robot->get_position(), QPoint(98, 98));
 }
 
 int main(int argc, char **argv) {
